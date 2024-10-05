39a1ad4b983118f9bdd6125e0d031193b8df3bfa
rnKerm <149631529+RayanHack5@users.noreply.github.com>
2024-10-05 21:22:42 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,8 +3,8 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION HERE",
-  PREFIX: process.env.PREFIX || '.',
+  SESSION_ID: process.env.SESSION_ID || "Kerm v3~umTDz8Ya",
+  PREFIX: process.env.PREFIX || '!',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
